d676cd526f5c149085bd77239d75dcd5e40bb50b
yuriel-v <emberbec@gmail.com>
2023-04-11 17:39:32 -0300

    Finish Python class

--- tripcode/src/main/java/dev/yuriel/spiceworks/tripcode/Python.java
+++ tripcode/src/main/java/dev/yuriel/spiceworks/tripcode/Python.java
@@ -4,21 +4,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class Python
 {
     private Process pythonInterpreter;
     private BufferedReader stdout;
     private PrintStream stdin;
+    private static final String charTable = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !@$%*()[]{}\\-=_+;:/?,.|";
+    private final Gson gson = new Gson();
 
     public Python() throws IOException
     {
-        this.pythonInterpreter = new ProcessBuilder("python3", "-c", "while True: exec(input())").redirectErrorStream(true).start();
+        boolean linux = System.getProperty("os.name").compareToIgnoreCase("linux") == 0;
+        this.pythonInterpreter = new ProcessBuilder(
+            "python" + (linux? "3" : ""),
+            "-c",
+            "while True: exec(input())")
+            .redirectErrorStream(true)
+            .start();
         this.stdout = new BufferedReader(new InputStreamReader(pythonInterpreter.getInputStream()));
         this.stdin = new PrintStream(pythonInterpreter.getOutputStream());
         if (!this.sanityCheck())
             System.err.println("Interpreter FAIL");
+        else
+            this.setupProducts();
     }
 
     private boolean sanityCheck() {
@@ -36,16 +52,46 @@ public class Python
         super.finalize();
     }
 
+    /**
+     * Sends a Python command to be processed by the underlying interpreter.
+     * 
+     * @param command The Python command to be executed.
+     * @return A string containing the command's return, or null if the command was not a print statement.
+     */
     public String sendCommand(String command)
     {
-        try {
+        try
+        {
             this.stdin.println(command);
             this.stdin.flush();
-            String result = this.stdout.readLine();
+            String result = command.startsWith("print(")? this.stdout.readLine() : null;
             return result;
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    private void setupProducts() {
+        this.sendCommand("from itertools import product");
+        this.sendCommand(String.format("char_table = '%s'", charTable));
+    }
+
+    public void generatePasswords(int chars) {
+        this.sendCommand(String.format("passwords = (''.join(i) for i in product(char_table, repeat=%d))", chars));
+    }
+
+    public synchronized List<String> getNextPasswords() { return this.getNextPasswords(50); }
+    public synchronized List<String> getNextPasswords(int howMany)
+    {
+        String passwords = this.sendCommand(String.format("print([next(passwords, None) for _ in range(%d)])", howMany));
+        if (passwords.startsWith("[None,"))
+            return List.of();
+        else
+        {
+            List<String> result = this.gson.fromJson(passwords, new TypeToken<ArrayList<String>>(){}.getType());
+            return result.stream().filter(p -> p.compareTo("None") != 0).collect(Collectors.toList());
         }
     }
 }
